f46535b8b213fa029cb70c6c910d26ee85641881
Luis <sansskeleto1121@gmail.com>
2022-07-23 19:21:21 -0500

    changes added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,256 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="page d-flex flex-column justify-content-center align-items-center">
+		<div class="main">
+			<form @submit.prevent="buscar()">
+				<div class="d-inline-flex flex-row align-items-baseline">
+					<MDBInput label="Pokemón a buscar" v-model="search" autofocus />
+					<MDBBtn
+						type="submit"
+						color="dark"
+						class="ms-3"
+						:disabled="!Boolean(search)"
+					>
+						<MDBSpinner v-if="loading" tag="span" size="sm" />
+						<div v-else>Buscar</div>
+					</MDBBtn>
+				</div>
+			</form>
+			<transition>
+				<div v-if="data" class="pokemon">
+					<div class="mt-5" style="padding: 50px">
+						<div
+							class="marco mx-auto d-flex justify-content-center align-items-center"
+						>
+							<img :src="data.sprites.front_default" alt="" />
+						</div>
+						<h3 class="align-self-center mt-4">{{ upper(data.name) }}</h3>
+						<div class="d-flex flex-column">
+							<div
+								class="d-inline-flex mb-2 justify-content-center align-items-center"
+							>
+								<b>Pokedex: </b>
+								<div class="ms-1">#{{ data.id }}</div>
+							</div>
+							<div class="d-inline-flex justify-content-center align-items-center">
+								<b>Tipo:</b>
+								<div class="ms-1">
+									{{
+										translate(data.types.map((x) => x.type.name))
+											.map((x) => upper(x))
+											.join(", ")
+									}}
+								</div>
+							</div>
+							<div class="mt-5">
+								<h4>Stats base:</h4>
+								<div>
+									<div class="row mt-2">
+										<div class="col">
+											<div class="w-50 mx-auto">
+												<div class="mt-2">
+													<b>Vida: {{ data.stats[0].base_stat }}</b>
+												</div>
+												<MDBProgress class="w-100">
+													<MDBProgressBar
+														:value="prcnt(data.stats[0].base_stat)"
+														bg="warning"
+													></MDBProgressBar>
+												</MDBProgress>
+											</div>
+										</div>
+									</div>
+									<div class="row mt-3">
+										<div class="col">
+											<div class="mt-2">
+												<b>Ataque: {{ data.stats[1].base_stat }}</b>
+											</div>
+											<MDBProgress class="w-100">
+												<MDBProgressBar
+													:value="prcnt(data.stats[1].base_stat)"
+													bg="warning"
+												></MDBProgressBar>
+											</MDBProgress>
+										</div>
+										<div class="col">
+											<div class="mt-2">
+												<b>Defensa: {{ data.stats[2].base_stat }}</b>
+											</div>
+											<MDBProgress class="w-100">
+												<MDBProgressBar
+													:value="prcnt(data.stats[0].base_stat)"
+													bg="warning"
+												></MDBProgressBar>
+											</MDBProgress>
+										</div>
+									</div>
+									<div class="row mt-4">
+										<div class="col">
+											<div class="mt-2">
+												<b>Ataque Especial: {{ data.stats[3].base_stat }}</b>
+											</div>
+											<MDBProgress class="w-100">
+												<MDBProgressBar
+													:value="prcnt(data.stats[1].base_stat)"
+													bg="warning"
+												></MDBProgressBar>
+											</MDBProgress>
+										</div>
+										<div class="col">
+											<div class="mt-2">
+												<b>Defensa especial: {{ data.stats[4].base_stat }}</b>
+											</div>
+											<MDBProgress class="w-100">
+												<MDBProgressBar
+													:value="prcnt(data.stats[1].base_stat)"
+													bg="warning"
+												></MDBProgressBar>
+											</MDBProgress>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</transition>
+		</div>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, inject } from "vue";
+import {
+	MDBBtn,
+	MDBInput,
+	MDBSpinner,
+	MDBProgress,
+	MDBProgressBar,
+} from "mdb-vue-ui-kit";
+import axios from "axios";
+const swal = inject("$swal");
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const loading = ref(false);
+const search = ref(null);
+const data = ref(null);
+
+const buscar = async () => {
+	loading.value = true;
+	data.value = null;
+
+	if (!search.value) {
+		loading.value = false;
+		return;
+	}
+
+	const busqueda = search.value.toLowerCase();
+
+	try {
+		const response = (
+			await Promise.all([
+				new Promise((resolve) => setTimeout(resolve, 500)),
+				axios.get(`https://pokeapi.co/api/v2/pokemon/${busqueda}`),
+			])
+		)[1];
+
+		data.value = response.data;
+
+		console.log(data.value);
+
+		loading.value = false;
+	} catch (e) {
+		data.value = null;
+		swal.fire({
+			icon: "error",
+			title: "Error",
+			text: "No encontramos un pokemon válido con ese nombre.",
+		});
+		loading.value = false;
+	}
+};
+
+const upper = (str) => {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const translate = (arr) => {
+	const dictionary = {
+		bug: "insecto",
+		dark: "siniestro",
+		dragon: "dragon",
+		electric: "electrico",
+		fairy: "hada",
+		fighting: "lucha",
+		fire: "fuego",
+		flying: "volador",
+		ghost: "fantasma",
+		grass: "planta",
+		ground: "tierra",
+		ice: "hielo",
+		normal: "normal",
+		poison: "veneno",
+		psychic: "psíquico",
+		rock: "roca",
+		steel: "acero",
+		water: "agua",
+	};
+
+	return arr.map((x) => dictionary[x] || x);
+};
+
+const prcnt = (x) => {
+	return Math.floor((x * 100) / 255);
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Roboto, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+html,
+body {
+	background: rgb(228, 228, 228);
+}
+
+.page {
+	height: 100vh;
+}
+
+.main {
+	background: white;
+	border-radius: 15px;
+	min-height: 70vh;
+	padding: 50px;
+	width: 600px;
+}
+
+.header{
+	max-width:600px
+}
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.pokemon {
+	background: rgb(255, 205, 112);
+	border-radius: 15px;
+}
+
+.marco {
+	width: 150px;
+	height: 150px;
+	border: 2px solid white;
+	border-radius: 100%;
+	background: rgb(255, 226, 158);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+import 'mdb-vue-ui-kit/css/mdb.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+createApp(App).use(VueAxios, axios).use(VueSweetalert2).mount('#app')
